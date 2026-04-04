e9bd7aa9482229ecf15c7506d2e699bf2c3affc5
Nikita M. <mna.dvlp@gmail.com>
2026-04-04 09:15:12 +0300

    App version

--- Charon.Dns/Program.cs
+++ Charon.Dns/Program.cs
@@ -23,7 +23,7 @@ namespace Charon.Dns;
 
 static class Program
 {
-    private const string AppVersion = "1.5.8";
+    private const string AppVersion = "1.5.9";
 
     public async static Task Main(string[] args)
     {
